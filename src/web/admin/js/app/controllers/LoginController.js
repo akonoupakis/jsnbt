197baf673e94711d0a830b830267e7ee0a22022f
Apostolis Konoupakis <konoupakis@gmail.com>
2014-09-16 23:44:27 +0300

    test login

---
+++ src/web/admin/js/app/controllers/LoginController.js
@@ -0,0 +1,27 @@
+﻿/* global angular:false */
+
+(function () {
+    "use strict";
+
+    angular.module("jsnbt")
+        .controller('LoginController', function ($scope, $location, $timeout, $logger, $q, $data) {
+           
+            var logger = $logger.create('LoginController');
+                             
+            $scope.credentials = {
+                username: '',
+                password: ''
+            };
+
+            $scope.login = function (credentials) {
+                console.log('login', credentials);
+                //AuthService.login(credentials).then(function (user) {
+                //    $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+                //    $scope.setCurrentUser(user);
+                //}, function () {
+                //    $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+                //});
+            };
+
+        });
+})();
---
+++ src/web/admin/js/app/directives/login.js
@@ -0,0 +1,33 @@
+﻿/* global angular:false */
+
+(function () {
+
+    "use strict";
+
+    angular.module('jsnbt')
+        .directive('login', function ($location) {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                scope: {
+                    ngModel: '='
+                },
+                template: '<div ng-if="visible" ng-include="\'tmpl/partial/login.html\'">',
+                link: function (scope, element, attrs) {
+                    element.addClass('login');
+
+                    var showDialog = function () {
+                        scope.visible = true;
+                    };
+
+                    scope.visible = false;
+                    scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
+                    scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
+
+                }
+            };
+
+        });
+
+})();
