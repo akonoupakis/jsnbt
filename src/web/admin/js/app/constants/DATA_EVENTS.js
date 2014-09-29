9a826a5ac2a470a58ae650534a4ec385cf9b40ea
Apostolis Konoupakis <konoupakis@gmail.com>
2014-09-29 22:13:31 +0300

    DATA_EVENTS

---
+++ src/web/admin/js/app/constants/DATA_EVENTS.js
@@ -0,0 +1,38 @@
+﻿/* global angular:false */
+
+(function () {
+    "use strict";
+
+    angular.module("jsnbt")
+        .constant('DATA_EVENTS', {
+
+            dataCreated: 'dataCreated',
+            dataUpdated: 'dataUpdated',
+            dataDeleted: 'dataDeleted',
+
+            draftCreated: 'draftsCreated',
+            draftUpdated: 'draftsUpdated',
+            draftDeleted: 'draftsDeleted',
+
+            languageCreated: 'languagesCreated',
+            languageUpdated: 'languagesUpdated',
+            languageDeleted: 'languagesDeleted',
+
+            nodeCreated: 'nodesCreated',
+            nodeUpdated: 'nodesUpdated',
+            nodeDeleted: 'nodesDeleted',
+
+            settingCreated: 'settingsCreated',
+            settingUpdated: 'settingsUpdated',
+            settingDeleted: 'settingsDeleted',
+
+            textsCreated: 'textsCreated',
+            textsUpdated: 'textsUpdated',
+            textsDeleted: 'textsDeleted',
+
+            usersCreated: 'usersCreated',
+            usersUpdated: 'usersUpdated',
+            usersDeleted: 'usersDeleted'
+
+        });
+})();
