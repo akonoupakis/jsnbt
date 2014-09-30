84afca5b3bab1a9efb9aaed507f6bd5d8c2a77de
Apostolis Konoupakis <konoupakis@gmail.com>
2014-09-30 19:39:29 +0300

    users

--- src/web/admin/js/app/controllers/UsersController.js
+++ src/web/admin/js/app/controllers/UsersController.js
@@ -4,9 +4,84 @@
     "use strict";
 
     angular.module("jsnbt")
-        .controller('UsersController', function ($scope, $rootScope, $location, $route, $timeout, $q, $logger) {
+        .controller('UsersController', function ($scope, $location, $logger, $q, $data, PagedDataService, ModalService, LocationService) {
             
             var logger = $logger.create('UsersController');
+
+            $scope.data = {};
+
+
+            var fn = {
+
+                load: function () {
+                    var deferred = $q.defer();
+
+                    PagedDataService.get(dpd.users.get, {}).then(function (response) {
+                        deferred.resolve(response);
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                    return deferred.promise;
+                },
+
+                create: function () {
+                    var deferred = $q.defer();
+
+                    //ModalService.open({
+                    //    title: 'Type a text key',
+                    //    controller: 'NamePromptController',
+                    //    template: 'tmpl/partial/modal/namePrompt.html'
+                    //}).then(function (result) {
+                    //    if (!!result && result !== '') {
+                    //        $data.texts.get({ key: result }).then(function (getResponse) {
+                    //            var first = _.first(getResponse);
+                    //            if (first) {
+                    //                deferred.resolve(first);
+                    //            }
+                    //            else {
+                    //                $data.texts.post($data.create('texts', { key: result })).then(function (response) {
+                    //                    deferred.resolve(response);
+                    //                }, function (error) {
+                    //                    deferred.reject(error);
+                    //                });
+                    //            }
+                    //        }, function (getError) {
+                    //            deferred.reject(getError);
+                    //        });
+                    //    }
+                    //});
+
+                    deferred.reject(new Error('not implemented'));
+
+                    return deferred.promise;
+                }
+
+            };
+
+
+            $scope.create = function () {
+                fn.create().then(function (result) {
+                    $location.next('/content/users/' + result.id);
+                }, function (ex) {
+                    logger.error(ex);
+                });
+            };
+
+            $scope.gridFn = {
+
+                edit: function (user) {
+                    $location.next('/content/users/' + user.id);
+                }
+
+            };
+
+
+            fn.load().then(function (response) {
+                $scope.data = response;
+            }, function (ex) {
+                logger.error(ex);
+            });
 
         });
 })(); 
