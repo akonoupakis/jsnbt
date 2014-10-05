ec906312ec3e4e88fbd4d4753a3346757abdc957
Apostolis Konoupakis <konoupakis@gmail.com>
2014-10-05 19:20:27 +0300

    language active and default validation

--- src/dpd/resources/languages/put.js
+++ src/dpd/resources/languages/put.js
@@ -7,9 +7,24 @@ var processFn = function () {
     if (!user.isAuthorized(me, 'languages', 'U'))
         cancel('access denied', 500);
 
-    var matched = dpdSync.call(dpd.languages.get, { code: self.code, id: { $nin: [self.id] } });
-    if (matched.length > 0)
-        cancel('language code already exists', 400);
+    if (changed('code')) {
+        var matched = dpdSync.call(dpd.languages.get, { code: self.code, id: { $nin: [self.id] } });
+        if (matched.length > 0)
+            cancel('language code already exists', 400);
+    }
+
+    if (changed('active')) {
+        if(!self.active && self.default)
+            error('active', 'cannot inactivate while default');
+    }
+
+    if (changed('default')) {
+        if (!self.default) {
+            var defaults = dpdSync.call(dpd.languages.get, { active: true, 'default': true, id: { $nin: [self.id] } });
+            if (defaults.length === 0)
+                error('default', 'cannot set to false as there are no other active default languages');
+        }
+    }
 
     emit('languageUpdated', self);
 };
