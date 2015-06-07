3b64ee5d5dc85f30030eb6da86479b08ffe86012
Apostolis Konoupakis <konoupakis@gmail.com>
2015-06-08 01:22:28 +0300

    angular directives for definition tables

---
+++ docs/js/app.js
@@ -0,0 +1,172 @@
+﻿/* global angular:false */
+
+(function () {
+    "use strict";
+
+    angular.module('jsnbt', [])
+        .directive('dcsContainer', function () {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                scope: {
+                    dcsTitle: '@',
+                    dcsSpyTitle: '@',
+                    dcsSpyLevel: '@'
+                },
+                templateUrl: 'tmpl/dcsContainer.html',
+                link: function (scope, element, attrs) {
+                    element.addClass('dcs-container');
+
+                    if (!scope.dcsSpyLevel)
+                        scope.dcsSpyLevel = 1;
+                }
+            };
+        })
+        .directive('dcsIntro', function () {
+
+             return {
+                 restrict: 'E',
+                 replace: true,
+                 transclude: true,
+                 scope: {
+                 },
+                 templateUrl: 'tmpl/dcsIntro.html',
+                 link: function (scope, element, attrs) {
+                     element.addClass('dcs-intro');
+
+                     if (!scope.dcsSpyLevel)
+                         scope.dcsSpyLevel = 1;
+                 }
+             };
+        })
+         .directive('dcsDefinition', function () {
+
+             return {
+                 restrict: 'E',
+                 replace: true,
+                 transclude: true,
+                 scope: {
+                 },
+                 templateUrl: 'tmpl/dcsDefinition.html',
+                 link: function (scope, element, attrs) {
+                     element.addClass('dcs-definition');
+
+                 }
+             };
+         })
+         .directive('dcsDefinitionTitle', function () {
+
+             return {
+                 restrict: 'E',
+                 replace: true,
+                 transclude: true,
+                 scope: {
+                     type: '@'
+                 },
+                 template: '<tr class="data-title"><td width="50%" ng-transclude></td><td width="50%">{{type}}</td></tr>',
+                 link: function (scope, element, attrs) {
+                     element.addClass('dcs-definition-title');
+
+                 }
+             };
+         })
+        .directive('dcsDefinitionDesc', function () {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                scope: {
+                },
+                template: '<tr class="data-description"><td colspan="2" ng-transclude></td></tr>',
+                link: function (scope, element, attrs) {
+                    element.addClass('dcs-definition-desc');
+
+                }
+            };
+        })
+        .directive('dcsDefinitionSample', function () {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                transclude: true,
+                scope: {
+                },
+                template: '<tr class="data-sample"><td colspan="2"><span class="code" ng-transclude></span></td></tr>',
+                link: function (scope, element, attrs) {
+                    element.addClass('dcs-definition-sample');
+
+                }
+            };
+        })
+        .config(function () {
+
+        });
+
+})();
+
+$(document).ready(function () {
+
+    angular.bootstrap(document, ['jsnbt']);
+
+    var randomIndex = 0;
+    var getSpyElements = function ($el, level) {
+        var results = [];
+
+        var spyItems = $('*[data-spy-title!=""][data-spy-level=' + level + ']', $el);
+        spyItems.each(function (i, item) {
+            randomIndex++;
+
+            var $item = $(item);
+
+            var itemId = 'spiedEl-' + level + '-' + randomIndex;
+
+            $item.prop('id', itemId);
+
+            var spyAnchor = $('<a />')
+                .prop('href', '#' + itemId)
+                .html($item.data('spy-title'));
+
+            var spyItem = $('<li />')
+                .addClass('level' + level).append(spyAnchor);
+
+            spyAnchor.click(function (e) {
+                e.preventDefault();
+
+                $('body').scrollTo('#' + itemId, { offset: -5, duration: 400 });
+            });
+
+            var childSpyElements = getSpyElements($item, level + 1);
+            if (childSpyElements.length > 0) {
+                var childUl = $('<ul />')
+                    .addClass('nav');
+
+                $(childSpyElements).each(function (ci, citem) {
+                    childUl.append(citem);
+                });
+
+                spyItem.append(childUl);
+            }
+
+            results.push(spyItem);
+        });
+
+        return results;
+    }
+   
+    setTimeout(function () {
+        var targetContainer = $('#scroll-spy-container');
+        var spyItems = getSpyElements(targetContainer, 1);
+        var targetUl = $('ul.nav.bs-docs-sidenav');
+
+        $(spyItems).each(function (i, item) {
+            targetUl.append(item);
+        });
+
+        $('#scroll-spy-sidebar').affix();
+    }, 500);
+
+});
