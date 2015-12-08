2a92b7a317b97fe49f7ab2ce20aaddeaa837ee99
Apostolis Konoupakis <konoupakis@gmail.com>
2015-12-08 21:16:56 +0200

    scroll spy bug

--- src/web/admin/js/core/cms/controls/ctrlScrollSpy.js
+++ src/web/admin/js/core/cms/controls/ctrlScrollSpy.js
@@ -10,10 +10,6 @@
             var ScrollSpyControl = function (scope, element, attrs) {
                 jsnbt.controls.ControlBase.apply(this, $rootScope.getBaseArguments(scope, element, attrs));
 
-                if (angular.scrollspy) {
-                    $('body').scrollspy('refresh');
-                }
-
                 var spy = function (cb) {
                     $('body').scrollspy({
                         target: '.bs-sidebar',
@@ -30,29 +26,35 @@
 
                 scope.$watch('ngModel', function (newValue, prevValue) {
 
-                    if (newValue && newValue.length > 0) {
+                    if (newValue && newValue.length > 0 && !_.isEqual(newValue, prevValue)) {
                         var activeId = element.find('li.active').data('target');
 
-                        spy(function () {
-
-                            if (!activeId)
-                                activeId = element.find('li:first').data('target');
-
-                            if (activeId) {
-                                var matched = element.find('li[data-target="' + activeId + '"]');
-                                if (matched.length === 1)
-                                    matched.addClass('active');
-                                else
-                                    element.find('li:first').addClass('active');
+                        setTimeout(function () {
+                            if (angular.scrollspy) {
+                                $('body').scrollspy('refresh');
                             }
+                            $('body').scrollTo(0, { offset: 0, duration: 0 });
+                            spy(function () {
 
-                            element.find('li a').click(function (e) {
-                                e.preventDefault();
+                                if (!activeId)
+                                    activeId = element.find('li:first').data('target');
 
-                                $('body').scrollTo($(this).attr('href'), { offset: -150, duration: 400 });
+                                if (activeId) {
+                                    var matched = element.find('li[data-target="' + activeId + '"]');
+                                    if (matched.length === 1)
+                                        matched.addClass('active');
+                                    else
+                                        element.find('li:first').addClass('active');
+                                }
+
+                                element.find('li a').click(function (e) {
+                                    e.preventDefault();
+
+                                    $('body').scrollTo($(this).attr('href'), { offset: -150, duration: 400 });
+                                });
+
                             });
-
-                        });
+                        }, 200);
                     }
                 });
             };
