f6f9ba548f1184df747307830020f43b7f9624a8
Apostolis Konoupakis <konoupakis@gmail.com>
2015-12-18 02:58:01 +0200

    removed installer

--- scripts/uninstall.js
+++
@@ -1,6 +0,0 @@
-var config = require('../package');
-var installer = require('../installer.js')
-
-console.log('uninstalling ' + config.name + ' v' + config.version);
-installer.npm.unpack(config.name);
-console.log('uninstalled ' + config.name + ' v' + config.version);
