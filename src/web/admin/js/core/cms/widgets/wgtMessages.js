b80d8a59f8d302bc8099b3a7f4b14551f7925de3
Apostolis Konoupakis <konoupakis@gmail.com>
2016-02-01 02:18:43 +0200

    messages widget

---
+++ src/web/admin/js/core/cms/widgets/wgtMessages.js
@@ -0,0 +1,51 @@
+﻿/* global angular:false */
+
+(function () {
+
+    "use strict";
+
+    angular.module('jsnbt')
+        .directive('wgtMessages', ['$rootScope', '$data', function ($rootScope, $data) {
+
+            var MessagesWidget = function (scope, element, attrs) {
+                element.addClass('wgt-messages');
+
+                scope.messages = [];
+
+                $data.messages.get({
+                    read: {
+                        $ne: scope.current.user.id
+                    },
+                    $sort: { timestamp: 1 }
+                }).then(function (response) {
+                    scope.messages = response;
+                }).catch(function (ex) {
+                    throw ex;
+                });
+
+                scope.dismiss = function (message) {
+                    $data.messages.put(message.id, {
+                        $push: {
+                            read: scope.current.user.id
+                        }
+                    }).then(function (response) {
+                        scope.messages = _.filter(scope.messages, function (x) { return x.id !== message.id });
+                    }).catch(function (ex) {
+                        throw ex;
+                    });
+                };
+
+            };
+
+            return {
+                restrict: 'E',
+                replace: true,
+                link: function (scope, element, attrs) {
+                    return new MessagesWidget(scope, element, attrs);
+                },
+                templateUrl: 'tmpl/core/widgets/wgtMessages.html'
+            };
+
+        }]);
+
+})();
---
+++ src/web/admin/js/core/cms/widgets/wgtWelcome.js
@@ -0,0 +1,25 @@
+﻿/* global angular:false */
+
+(function () {
+
+    "use strict";
+
+    angular.module('jsnbt')
+        .directive('wgtWelcome', ['$rootScope', function ($rootScope) {
+
+            var WelcomeWidget = function (scope, element, attrs) {
+                element.addClass('wgt-welcome');
+            };
+
+            return {
+                restrict: 'E',
+                replace: true,
+                link: function (scope, element, attrs) {
+                    return new WelcomeWidget(scope, element, attrs);
+                },
+                templateUrl: 'tmpl/core/widgets/wgtWelcome.html'
+            };
+
+        }]);
+
+})();
